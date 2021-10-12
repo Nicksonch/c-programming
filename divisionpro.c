40ad87c60dbb696469c7e32fc6065f8e71c835d3
Nicksonch <91759277+Nicksonch@users.noreply.github.com>
2021-10-12 10:55:14 +0300

    Add files via upload

---
+++ divisionpro.c
@@ -0,0 +1,13 @@
+/*Arithmetic Operator division*/
+#include <stdio.h>
+int main ()
+{
+int a, b, divide;
+printf("/n ,enter A:");
+scanf ("%d", &a);
+printf ("/n,enter B:");
+scanf ("%d",&b);
+divide=a/b;
+printf ("/n,divide is %d",divide);
+return 0;
+}
