72105f5ee98ad55650f1a64efd45ea5a24da79da
Nicksonch <91759277+Nicksonch@users.noreply.github.com>
2021-10-22 12:15:15 +0300

    Update relational operators.c

--- relational operators.c
+++ relational operators.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main()
 {
-    int a = 9, b = 9, c = 11
+    int a = 9, b = 9, c = 11;
     printf("%d == %d is %d\n", a, b, a == b);
     printf("%d != %d is %d\n", b, a, b != a);
     printf("%d > %d is %d\n", a, b, a > b);
