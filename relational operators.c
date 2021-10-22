eeba72623abe1530e1a142e8117983f6bd63b5e0
Nicksonch <91759277+Nicksonch@users.noreply.github.com>
2021-10-22 12:14:09 +0300

    Add files via upload

---
+++ relational operators.c
@@ -0,0 +1,14 @@
+/*relational operators in c */
+#include <stdio.h>
+int main()
+{
+    int a = 9, b = 9, c = 11
+    printf("%d == %d is %d\n", a, b, a == b);
+    printf("%d != %d is %d\n", b, a, b != a);
+    printf("%d > %d is %d\n", a, b, a > b);
+    printf("%d < %d is %d\n", b, c, b < c);
+    printf("%d >= %d is %d\n", c, b, c >= b);
+    printf("%d <= %d is %d\n", b, c, b <= c);
+
+    return 0;
+}
