e63e40660d6e8b70d1f954430a78903f71aa6fbb
Nicksonch <91759277+Nicksonch@users.noreply.github.com>
2021-10-22 14:09:00 +0300

    Add files via upload

---
+++ increament and decreament operators.c
@@ -0,0 +1,12 @@
+/*Increment and decrement Operators*/
+   #include <stdio.h>
+   int main()
+   {
+       int a=8, b=9;
+    printf("\n %d \t", ++a);
+    printf("\n %d \t", --a);
+    printf("\n %d \t", --b);
+    printf("\n %d \t", ++b);
+    printf("\n %d \t", ++a);
+    return 0;
+   }
