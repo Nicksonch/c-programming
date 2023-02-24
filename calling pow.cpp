0c80d5d7417781dfc0718d8d50372f57904e7777
Nicksonch <91759277+Nicksonch@users.noreply.github.com>
2023-02-24 12:23:02 +0300

    Add files via upload

---
+++ calling pow.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double x=2.5, y;
+	y=pow(x,3);
+	cout<<"2.5 raised to 3 yields";
+	cout<<y<< endl;
+	cout<<"2+(5 raised to the power 2.5 yields";
+	cout<<2+pow(5.0,x)<<endl;
+	return 0;
+}
---
+++ nickson calling.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int sum(int number1, int number2){
+int number3=number1+number2;
+return number3;}
+int main(){
+	cout<<("the sum of numbers is");
+	cout<<sum(50,40);
+}
